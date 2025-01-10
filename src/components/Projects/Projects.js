63f2c42e368fd9a03da7dbecc97e20dba947481a
rahimkhan07 <140167765+rahimkhan07@users.noreply.github.com>
2025-01-10 23:16:05 +0530

    add in my repo

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
+import weather from "../../Assets/Projects/weather.png";
+
+import ecom from "../../Assets/Projects/e-com.png";
 import recipe from "../../Assets/Projects/Recipe-App.png";
-import suicide from "../../Assets/Projects/suicide.png";
+import rentcar from "../../Assets/Projects/rentcar.png";
 import newsapp from "../../Assets/Projects/newsapp.png";
 
 function Projects() {
@@ -43,12 +43,12 @@ function Projects() {
             />
           </Col>
 
-          {/* <Col md={4} className="project-card">
+           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={ecom}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              title="E-commerce App"
+              description="An E-commerce web app built using ReactJS, HTML, CSS, and Bootstrap offers responsive design, user-friendly interfaces, dynamic product listings, and efficient cart management for seamless online shopping experiences."
               ghLink=""
               demoLink=""              
             />
@@ -56,26 +56,27 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={weather}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
+              title="Weather App"
+              description="A Weather web app built with ReactJS, HTML, and CSS integrates a Weather API to provide real-time weather updates, current date, and time. It offers responsive design and accurate forecasts for user convenience."
               ghLink=""
               demoLink=""
             />
           </Col>
 
+
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={rentcar}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              title="RentCar Website"
+              description="A rent car website built with PHP, HTML, CSS, Bootstrap, and MySQL provides responsive design, car listings, booking forms, secure user login, real-time availability checks, and a database for managing rentals and user data."
               ghLink=""
               // demoLink="" <--------Please include a demo link here
             />
           </Col>
-
+{/*
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={emotion}
